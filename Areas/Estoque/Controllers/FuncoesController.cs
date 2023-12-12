5887dde92332a7f9a00d2f5a85de60d5ee697f05
Edson Candido <Edson.Candido@howden.com>
2023-12-12 13:21:30 -0300

    adicionado link para funções de usuarios

---
+++ Areas/Estoque/Controllers/FuncoesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesenvolvedorNET.Areas.Estoque.Controllers
+{
+    [Area("estoque")]
+    [Route("estoque/[controller]")]
+    public class FuncoesController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return RedirectToRoute(new {controller="home", action="index", area="" });
+        }
+    }
+}
