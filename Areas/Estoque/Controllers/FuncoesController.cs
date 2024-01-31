0406b7bb0378a7c9fa62806e734b05062a5a1983
Edson Candido <Edson.Candido@howden.com>
2024-01-31 13:18:10 -0300

    obtendo a lista de funções a mostrando na view

--- Areas/Estoque/Controllers/FuncoesController.cs
+++ Areas/Estoque/Controllers/FuncoesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DesenvolvedorNET.Areas.Estoque.Controllers
 {
@@ -9,7 +10,22 @@ namespace DesenvolvedorNET.Areas.Estoque.Controllers
         
         public IActionResult Index()
         {
-            return View();
+            // cria um model de mock para trabalhar no render dessa view quero mostrar numa tabela 
+            // os nomes das funcoes do RoleManager
+
+            //List<IdentityRole> roles = new List<IdentityRole>();
+            //// Adicione os objetos IdentityRole à lista
+            //// Exemplo:
+            //roles.Add(new IdentityRole("Administrador"));
+            //roles.Add(new IdentityRole("Usuario"));
+
+            var _roleManager = HttpContext.RequestServices.GetRequiredService<RoleManager<IdentityRole>>();
+
+
+            //get roles from RoleManager
+            var roles = _roleManager.Roles.ToList();
+
+            return View(roles);
             //return RedirectToRoute(new {controller="home", action="index", area="" });
         }
     }
